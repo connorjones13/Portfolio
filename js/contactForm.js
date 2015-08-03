167be0ac7b5cd8a2afd501b20283fad6e2e6005f
Connor Jones <connorjones135@gmail.com>
2015-08-03 11:59:28 -0500

    reading data from json file

---
+++ js/contactForm.js
@@ -0,0 +1,48 @@
+
+function submitForm() {
+    var email = $('#contact-email').val();
+    var subject = $('#email-subject').val();
+    var message = $('#email-body').val();
+
+    console.log(email);
+
+    var form = document.getElementsByName('contact-form')[0];
+
+
+    if(email.indexOf('@') === -1 || email.indexOf('.') === -1) {
+        $('#alertDiv').append('<div class="alert alert-danger" role="alert">' +
+            '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+            '<span aria-hidden="true">&times;</span></button>Please enter a valid email address.</div>');
+        return false;
+    }
+    else if(subject.length < 5){
+        $('#alertDiv').append('<div class="alert alert-danger" role="alert">' +
+            '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+            '<span aria-hidden="true">&times;</span></button>Please enter a valid subject.</div>');
+        return false;
+    }
+    else if(message === ''){
+        $('#alertDiv').append('<div class="alert alert-danger" role="alert">' +
+            '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+            '<span aria-hidden="true">&times;</span></button>You don\'t really want to send me a blank email, do you?</div>');
+        return false;
+    }
+    else {
+        $('#alertDiv').append('<div class="alert alert-success" role="alert">' +
+            '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+            '<span aria-hidden="true">&times;</span></button>Your email has been sent!</div>');
+    }
+
+    form.submit();
+    //todo: send email using mail client & authenticate
+
+    //todo: popup window confirming email was sent
+
+    form.reset(); // clears form
+
+
+
+
+
+    return false;
+}
---
+++ js/resumeBuilder.js
@@ -0,0 +1,21 @@
+$.getJSON('assets/work.json', function displayWork(work) {
+    for(var job in work.jobs) {
+        $("#workExperience").append(HTMLworkStart);
+
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+        var formattedEmployerTitle = formattedEmployer + formattedTitle;
+        $(".work-entry:last").append(formattedEmployerTitle);
+
+        var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+        $(".work-entry:last").append(formattedDates);
+
+        var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+        $(".work-entry:last").append(formattedLocation);
+
+        var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+        $(".work-entry:last").append(formattedDescription);
+    }
+});
+
+//displayWork();
