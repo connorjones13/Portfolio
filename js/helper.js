ed7406bcc334e61f8cd03c40c1c0a3849183298d
Connor Jones <connorjones135@gmail.com>
2015-08-09 20:32:16 -0500

    added platform experience

--- js/helper.js
+++ js/helper.js
@@ -1,10 +1,12 @@
 var HTMLworkStart = '<div class="work-entry"></div>';
-var HTMLworkEmployer = '<a href="#">%data%';
-var HTMLworkTitle = ' - %data%</a>';
+var HTMLworkEmployer = '<h4 class="employer-name">%data%';
+var HTMLworkTitle = ' - %data%</h4>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
-var HTMLworkDescription = '<p><br>%data%</p>';
+var HTMLworkDescription = '<p>%data%</p>';
 
-var HTMLlanguageStart = '<div class="skill-entry progress"></div>';
-var HTMLlanguageSpecs = '<div class="progress-bar" role="progressbar" aria-valuenow="%data%" ' +
+var HTMLlanguageStart = '<div class="language-entry progress"></div>';
+var HTMLprogressSpecs = '<div class="progress-bar" role="progressbar" aria-valuenow="%data%" ' +
     'aria-valuemin="0" aria-valuemax="100" style="width: %width%%; background-color: %color%; background-image: none;">%name%</div>';
+
+var HTMLplatformStart = '<div class="platform-entry progress"></div>';
